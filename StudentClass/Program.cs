df05c169cac22bc50a07dca9b6cea8171f51f062
VaniaStoicheva <cecostoi4ev@abv.bg>
2016-06-06 17:22:56 +0300

    C# OOP

--- StudentClass/Program.cs
+++ StudentClass/Program.cs
@@ -10,6 +10,16 @@ namespace StudentClass
     {
         static void Main(string[] args)
         {
-        }
+            Student student = new Student("Peter", 22);
+            student.PropertyChanged += (sender, eventArgs) =>
+            {
+                Console.WriteLine("Property changed: {0} (from {1} to {2})",
+                    eventArgs.PropertyName, eventArgs.OldValue, eventArgs.NewValue);
+            };
+            student.Name = "Maria";
+            student.Age = 19;
+
+        
+    }
     }
 }
